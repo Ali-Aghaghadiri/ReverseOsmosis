67672be78f2eee71ad0097ea05834687d6c987d9
Ali AghaGhadiri <80603584+Ali-Aghaghadiri@users.noreply.github.com>
2023-10-24 11:01:08 +0330

    Update ROSystem.py
    Generalizing system generation using just a unit cell , desired system size and molecules.

--- ROSystem.py
+++ ROSystem.py
@@ -1,11 +1,14 @@
 import argparse
+from math import ceil
 from ase.io import read
 import numpy as np
 import subprocess
 import tempfile
 import os
 from ase.visualize import view
+from MyCrystal import drill, make_orthogonal
 from solution import solvate
+from ase.atoms import Atoms
 
 
 # Argument parser
@@ -29,17 +32,18 @@ ap.add_argument("-o", "--out", default="./ROSystem.pdb", help="Output file.")
 ap.add_argument("-t", "--tolerance", default=2.0, help="Set tolerance.")
 ap.add_argument("--show", action="store_true", help="View the packing result.")
 
-membrane_ap.add_argument(
-    "-m", "--membrane", required=True, help="Membrane file.")
-piston_ap.add_argument("-p", "--piston", required=True, help="Piston file.")
+membrane_ap.add_argument("-m", "--membrane", help="Membrane file.")
+membrane_ap.add_argument("-d", "--drill", type=float, help="Diameter of the membrane hole.")
+piston_ap.add_argument("-p", "--piston", help="Piston file.")
 mixture_ap.add_argument("-M", "--molecule", nargs=2, dest="molecules", action="append",
                         required=True, help="Mixture compounds and concentration.")
-system_ap.add_argument("-s", "--separation", required=True, type=float,
-                       help="Separation of the piston and membrane.")
+system_ap.add_argument("-s", "--separation", type=float, help="Separation of the piston and membrane.")
+system_ap.add_argument("-c", "--crystal", type=str, help="Unit cell crystal file.")
+system_ap.add_argument("--size", nargs=3, type=float, help="Size of the chamber.")
 
 
-def pack(packmol: str, membrane: str, piston: str, separation: float,
-         molecules: list[str, float], output: str, tolerance=2.0):
+def pack(packmol: str, membrane: str | Atoms, piston: str | Atoms, separation: float,
+         molecules: list[str, float], output: str, temperature: float = 298.15, tolerance=2.0):
     """
     Insert mixture molecules using `packmol` executable.
     
@@ -62,7 +66,10 @@ def pack(packmol: str, membrane: str, piston: str, separation: float,
     solution_output = os.path.join(directory, solution_name)
     molecules = {k: float(v) for k, v in molecules}
     # find center of the membrane.
-    membrane_atoms = read(membrane)
+    if not isinstance(membrane, Atoms):
+        membrane_atoms = read(membrane)
+    else:
+        membrane_atoms = membrane
     membrane_maximum = membrane_atoms.positions.max(axis=0)
     membrane_minimum = membrane_atoms.positions.min(axis=0)
     xc, yc, _ = (membrane_maximum + membrane_minimum) / 2
@@ -70,7 +77,7 @@ def pack(packmol: str, membrane: str, piston: str, separation: float,
     # calculate size of the system and generate a mixture.
     dx, dy, _ = membrane_maximum - membrane_minimum
     dz = separation
-    solvate(dx, dy, dz, tolerance, output=solution_output,
+    solvate(dx, dy, dz, temperature, tolerance, output=solution_output,
             packmol=packmol, **molecules)
     # translate the bottom of the mixture on top of membrane.
     solution_atoms = read(solution_output)
@@ -81,7 +88,10 @@ def pack(packmol: str, membrane: str, piston: str, separation: float,
     solution_shift = (xc - sx, yc - sy, zc - sz + (tolerance))
     solution_atoms.translate(solution_shift)
     # translate the bottom of the piston on top of the mixture.
-    piston_atoms = read(piston)
+    if not isinstance(piston, Atoms):
+        piston_atoms = read(piston)
+    else:
+        piston_atoms = piston
     piston_maximum = piston_atoms.positions.max(axis=0)
     piston_minimum = piston_atoms.positions.min(axis=0)
     px, py, _ = (piston_maximum + piston_minimum) / 2
@@ -135,21 +145,61 @@ def lammps_data(ROSystem: str):
     print(stdout)
 
 
+def mklyr(crystal_info: str, size: list[float], drill_radius: float = 0.0):
+    crystal = read(crystal_info)
+    a, b, c, *_ = crystal.get_cell_lengths_and_angles()
+    A = ceil(size[0] / a)
+    B = ceil(size[1] / b)
+    layer = crystal * [A, B, 1]
+    # TODO: Trim the edges to get the proper size.
+    make_orthogonal(layer)
+    if drill_radius:
+        drill(layer, drill_radius)
+    return layer
+
+
+def mksystem(unit_cell: str, size: list[float], hole_diameter: float, 
+             packmol: str, output: str, tolerance: float, 
+             temperature: float = 298.15, **molecules):
+    hole_radius = hole_diameter / 2
+    piston = mklyr(unit_cell, size)
+    membrane = mklyr(unit_cell, size, hole_radius)
+    molecules = [[m, c] for m, c in molecules.items()]
+    pack(packmol, membrane, piston, size[2], molecules, output, temperature, tolerance)
+    lammps_data(output)
+
+
 if __name__ == "__main__":
     arguments = ap.parse_args()
     print(arguments)
 
-    pack(
-        packmol=arguments.executable,
-        membrane=arguments.membrane,
-        piston=arguments.piston,
-        separation=arguments.separation,
-        molecules=arguments.molecules,
-        output=arguments.out,
-        tolerance=arguments.tolerance
-    )
+    if arguments.membrane and arguments.piston:
 
-    lammps_data(arguments.out)
+        pack(
+            packmol=arguments.executable,
+            membrane=arguments.membrane,
+            piston=arguments.piston,
+            separation=arguments.separation,
+            molecules=arguments.molecules,
+            output=arguments.out,
+            temperature=arguments.temperature,
+            tolerance=arguments.tolerance
+        )
+
+        lammps_data(arguments.out)
+    
+    elif arguments.crystal and arguments.size:
+        molecules = {k: float(v) for k, v in arguments.molecules}
+        mksystem(
+            unit_cell=arguments.crystal,
+            size=arguments.size,
+            hole_diameter=arguments.drill,
+            packmol=arguments.executable,
+            output=arguments.out,
+            tolerance=arguments.tolerance,
+            temperature=arguments.temperature,
+            **molecules
+        )
 
     if arguments.show:
         system_pdb = read(arguments.out)
